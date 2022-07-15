f1dd91eff9ad5f05c01f5491165bcb6b38fabf81
Tung Lam <mvvj2@MVVJ2s-MacBook-Pro.local>
2022-07-15 11:14:45 +0700

    Add SS10 + update SS9

---
+++ SS10_OH/BTVN.js
@@ -0,0 +1,130 @@
+//Bai 7.1
+// getString = (string) => {
+//     let totalStr = '';
+//     let newstring = string.split(' ')
+//     let newstring2 = newstring.join('');
+//     for(let i = 0; i < newstring2.length; i++){
+//        totalStr += newstring2[i] + ': o vi tri ' + i + '\n';
+//     }
+//     return totalStr;
+// }
+
+//Bai 7.1 
+// getString = (string) => {
+//     let totalStr = '';
+//     let str1 = ''
+//     for(let i = 0; i < string.length; i++){
+//         if(string[i] == ' '){
+//             str1 += string.splice(i, 1);
+//         };
+//         totalStr += string[i] + " o vi tri la: " + i + "\n";
+//     }
+//     return totalStr;
+// }
+// console.log(getString("Day la ha noi"))
+
+// let string = 'Đây là nội dung';
+// console.log(getString(string))
+
+//Bai 7.2
+// tongBinhPhuong = () => {
+//   let total = 0;
+//   for (let i = 0; i <= 100; i++) {
+//     if (i % 5 == 0) {
+//       total += i * i;
+//     }
+//   }
+//   return total;
+// };
+// console.log(tongBinhPhuong());
+
+//Bai 3:
+// mangChia5 = () => {
+//     let total = '';
+//     for(let i = 0; i <= 100; i++){
+//         if(i % 5 == 0){
+//             total += i + ' ';
+//         }
+//     }
+//     return total;
+// }
+// console.log(mangChia5());
+
+//Bai 7.4
+// timKytu = (string, char) => {
+//     let total = '';
+//     for(let i = 0; i < string.length; i++){
+//         if(string[i] == char){
+//             total += i + ' ';
+//         }
+//     }
+//     if(total == ''){
+//         return 'Khong tim thay ky tu ' + char;
+//     }
+//     return total;
+// }
+// let string = 'anh yeu em nhieu lam'
+// let char = 'r'
+// console.log(timKytu(string, char));
+
+//Bai 7.5.1
+
+// daoKytu = (string) => {
+//     let total = '';
+//     for(let i = string.length-1 ;i>=0; i--){
+//         total += string[i] + ' ';
+//     }
+//     return total;
+// }
+// console.log(daoKytu("hello"))
+
+//Bai 7.6
+// sosanhArray = (arr1,arr2) => {
+//     let total1 = 0,total2=0;
+//     for(let i = 0; i < arr1.length; i++){
+//         total1 += arr1[i];
+//     }
+//     for(let i = 0; i <arr2.length; i++){
+//         total2 += arr2[i];
+//     }
+//     return total1 > total2 ? "Mang dau tien lon hon" : "Mang dau tien nho hon";
+// }
+// console.log(sosanhArray([1,2,3,4,10],[3,4,5,7]))
+
+//Bai 7.7      quá nhiều bug
+// insertValue = (string, char, newChar) => {
+//     let flag = '';
+//     for(let i=0; i<string.length; i++){
+//         if(string[i] == char){
+//             flag = i;
+//             break;
+//         }
+//     }
+//     let substr = string.splice(flag, string.length-flag);
+//     let finalStr = string + "," + newChar + "," + substr.splice(substr.length-1,1);
+//     return finalStr;
+// }
+// console.log(insertValue(['hoa','hua','khai','nice','five'],'khai',' congchua '))
+
+//Bai 7.7:
+// insertValue = (string, char, char2) => {
+//     let flag = 0;
+//     for(let i=0; i<string.length; i++){
+//         if(string[i] == char){
+//             flag += i;
+//             break;
+//         }
+//         console.log(flag)
+//     let newstr = string.splice(flag, 1, char2);
+//     return string;
+// }
+// }
+
+// console.log(insertValue(['hoa','hien','chien','toan'],'chien','hieu'))
+let string = ['Lan', 'Điệp', 'Ngọc','Ngạn', 'Trinh']
+newstring = string.splice(4,1,"An",'Trinh');
+console.log(string)
+
+
+
+
---
+++ SS10_OH/index.js
@@ -0,0 +1,46 @@
+// console.log('This is Game')
+
+
+// hello = () => 'Hello World';
+// console.log(hello())
+
+
+// tinhTong = (a,b) => a + b;
+// console.log(tinhTong(3,4));
+
+
+
+// tinhTong = (a,b) => a + b;
+
+
+
+//Using Map() to use a function with array
+// let arr = [1,2,3,4]
+// function SumItemArr(item, index) {
+//     return item + index;
+// }
+
+// arr.push(5);
+// console.log(arr.map(SumItemArr))
+
+
+
+// let arr1 = [1,2,3,4,5]
+// function MultiItemArr(item){
+//     return item * 2;
+// }
+// MultiItemArr = item => item * 2;
+
+// console.log(arr1.map(MultiItemArr))
+
+
+// let stringg = "     Hello World     ";
+// console.log(stringg.trim());
+
+let String = ['avenger', 'black widow']
+
+str1 = (item) => {
+    return item.charAt(0).toUpperCase();
+}
+console.log(String.map(str1))
+
